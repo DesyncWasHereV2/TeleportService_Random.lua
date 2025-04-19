5e3aff6670dba141daaab2537b77192a3004c9cc
DesyncWasHereV2 <desync.develeoper@gmail.com>
2025-04-19 02:36:22 +0100

    Create Bass.lua

---
+++ Bass.lua
@@ -0,0 +1,29 @@
+local function getRandomJobId()
+    while true do
+        local success, data = pcall(function()
+            return game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+        end)
+
+        if success and data and data.data then
+            local servers = data.data
+            if #servers > 0 then
+                local randomServer = servers[math.random(1, #servers)]
+                local jobId = randomServer.id
+                print("Random JobId: " .. jobId)
+                return jobId
+            end
+        else
+            print("Data invalid or error, retrying...")
+        end
+        
+        task.wait(60)
+    end
+end
+
+local RandomJobId = getRandomJobId()
+
+if RandomJobId then
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, RandomJobId, game.Players.LocalPlayer)
+else
+    print("Failed to retrieve a valid JobId.")
+end
